0a00f2f7bda4ccfc27d4af82213e68aaecec1d67
David Dormagen <czapper@gmx.de>
2020-07-21 18:25:23 +0200

    updated default config

--- lorryserver/defaultconfig.py
+++ lorryserver/defaultconfig.py
@@ -6,3 +6,26 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 OWN_HOST = "localhost"
 RESOURCES_PATH = "/some/local/path/"
 TEST_DATA_PATH = "/some/local/path/containing/at/least/three/testfiles/"
+
+DEBUG = False
+SECRET_KEY = b'fillmewithdata'
+SESSION_COOKIE_SAMESITE = "Lax"
+
+SQLALCHEMY_DATABASE_URI = "postgres://user:password@localhost:5432/databasename"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Note that the simple in-memory cache is not shared between worker threads.
+# That means that different connections to the website might yield different cache states.
+# So we combine that with a short cache timeout.
+CACHE_TYPE = "simple"
+CACHE_DEFAULT_TIMEOUT = 60
+
+OWN_HOST = "localhost"
+RESOURCES_PATH = "/some/local/path/"
+TEST_DATA_PATH = "/some/local/path/containing/at/least/three/testfiles/"
+ALLOWED_FILE_EXTENSIONS = ("ocs", "ocf", "ocd")
+
+#SSO_ENDPOINT = "https://example.com/"
+SSO_ENDPOINT = None
+
+SSO_HMAC_SECRET = "some secret"
